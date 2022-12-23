552c45a5952239b3cd852fc0416181d79351f512
carvalhoju <julio.deoliveira-carvalho@jsmd-group.com>
2022-12-23 03:10:02 +0100

    implemented theme, BaseLine and Global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React from "react";
-import { Home } from "./components/Home";
+import { Home } from "./components";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  createTheme,
+  CssBaseline,
+  GlobalStyles,
+  ThemeProvider,
+} from "@mui/material";
 
 const client = new ApolloClient({
   uri: "http://coding-challenge-a8s934ksd.eu-central-1.elasticbeanstalk.com/graphql",
   cache: new InMemoryCache(),
 });
 
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 function App(): JSX.Element {
   return (
     <ApolloProvider client={client}>
-      <Home />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <GlobalStyles styles={{ body: { padding: theme.spacing(3) } }} />
+        <Home />
+      </ThemeProvider>
     </ApolloProvider>
   );
 }
